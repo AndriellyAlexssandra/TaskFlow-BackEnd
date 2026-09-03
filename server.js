5f08307d97da5fa6e3deefe45040997f98498131
Andrielly alexssandra santos de paula <andrielly.paula@aluno.senai.br>
2026-09-03 11:29:51 -0300

    Organização MVC - implementação do modal - dia 4

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const usuariosRoutes = require("./src/routes/usuarios.routes");
 const tarefasRoutes = require("./src/routes/tarefas.routes");
+const projetosRoutes = require("./src/routes/projetos.routes");
 
 const app = express();
 const PORTA = 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/usuarios", usuariosRoutes);
 app.use("/tarefas", tarefasRoutes);
+app.use("/projetos", projetosRoutes);
 
 
 app.get("/", (req, res) => {
