1ba2c9a5fba0ed147871cad9bf85cf9dedb18512
Andrielly alexssandra santos de paula <andrielly.paula@aluno.senai.br>
2026-09-10 11:31:51 -0300

    Adiciona validação de dados e esquemas para tarefas e usuários

--- src/routes/tarefas.routes.js
+++ src/routes/tarefas.routes.js
@@ -3,12 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 const tarefasController = require("../controllers/tarefas.controller");
+const validar = require("../middlewares/validar");
+const schemas = require("../middlewares/schemas");
+
 router.get("/estatisticas", tarefasController.estatisticas);
 router.get("/estatisticas/resumo", tarefasController.estatisticasResumo);
 router.get("/", tarefasController.listarTarefas);
 router.get("/:id", tarefasController.buscarTarefaPorId);
-router.post("/", tarefasController.criarTarefa);
-router.put("/:id", tarefasController.atualizarTarefa);
+router.post("/", validar(schemas.tarefa), tarefasController.criarTarefa);
+router.put("/:id", validar(schemas.tarefa), tarefasController.atualizarTarefa);
 router.delete("/:id", tarefasController.deletarTarefa); 
 
 module.exports = router;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,10 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 const usuariosController = require("../controllers/usuarios.controller");
+const validar = require("../middlewares/validar");
+const schemas = require("../middlewares/schemas");
+
 router.get("/", usuariosController.listarUsuarios);
 router.get("/:id", usuariosController.buscarUsuarioPorId);
-router.post("/", usuariosController.criarUsuario);
-router.put("/:id", usuariosController.atualizarUsuario);
+router.post("/", validar(schemas.usuario), usuariosController.criarUsuario);
+router.put(
+  "/:id",
+  validar(schemas.usuario),
+  usuariosController.atualizarUsuario,
+);
 router.delete("/:id", usuariosController.deletarUsuario);
 
 module.exports = router;
