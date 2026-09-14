0742645ef74d3246dd15a8ec51910f0c12327e96
Andrielly alexssandra santos de paula <andrielly.paula@aluno.senai.br>
2026-09-14 11:09:07 -0300

    Semana 11 Dia 4 —  bug persistente(corrigir!)

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 
-
 const app = express();
 const PORTA = process.env.PORTA || 3001;
 
-const usuariosRoutes = require("./src/routes/usuarios.routes");
+const authRoutes = require("./src/routes/auth.routes");
+const autenticar = require("./src/middlewares/autenticar");
 const tarefasRoutes = require("./src/routes/tarefas.routes");
+const usuariosRoutes = require("./src/routes/usuarios.routes");
 const projetosRoutes = require("./src/routes/projetos.routes");
-const authRoutes = require('./src/routes/auth.routes');
 
-const logger = require("./src/middlewares/logger");
 const validarContentType = require("./src/middlewares/validarContentType");
-
+const logger = require("./src/middlewares/logger");
 
 const cors = require("cors");
 app.use(
@@ -22,23 +21,24 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     maxAge: 86400,
-  }));
+  }),
+);
 
 app.use(express.json());
 app.use(validarContentType);
 app.use(logger);
-app.use('/auth', authRoutes);
-
-app.use("/usuarios", usuariosRoutes);
-app.use("/tarefas", tarefasRoutes);
-app.use("/projetos", projetosRoutes);
 
 app.get("/", (req, res) => {
   res.json({ api: "TaskFlow", versao: "1.0", status: "online" });
 });
 
+app.use("/usuarios", autenticar, usuariosRoutes);
+app.use("/tarefas", autenticar, tarefasRoutes);
+app.use("/projetos", autenticar, projetosRoutes);
+app.use("/auth", authRoutes);
+
 app.use((req, res) => {
-  res.status(404).json({ erro: "Rota não encontrada :( " });
+  res.status(404).json({ erro: "caiu rota nao encontrada :( " });
 });
 
 app.listen(PORTA, () => {
